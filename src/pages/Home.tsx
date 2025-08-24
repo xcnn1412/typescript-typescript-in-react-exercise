fe9a7058939f3b8905c305b0d04437cf897b42ba
nattawara <nattawara.jtk@gmail.com>
2025-08-24 21:39:04 +0700

    add init code

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
+// TODO1: กำหนด Type สำหรับ prop 'name'
+
+const Home = ({ name }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold mb-6">Welcome, {name}!</h1>
+      <button
+        onClick={() => navigate("/users")}
+        className="px-4 py-2 bg-blue-500  rounded hover:bg-blue-600 transition"
+      >
+        Go to Users List
+      </button>
+    </div>
+  );
+};
+
+export default Home;
